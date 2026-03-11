9cc1703a288b7f5f09e93d0674a215f4bbc1f111
Ankit Jaiswal <ankitjaiswal3016@gmail.com>
2026-03-11 11:08:45 +0530

    Add User model with password hashing and JWT authentication middleware

---
+++ Backend/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true
+});
+
+// Validate password
+UserSchema.methods.checkPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
